d13440a52f5e0f3fc0276920258620df985c9006
Martin Fučík <mar.fucik@gmail.com>
2020-11-04 15:38:11 +0100

    MyNetworkManager implemented (inherit from Mirrors NetworkManager)

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+	public override void OnClientConnect(NetworkConnection conn)
+	{
+		base.OnClientConnect(conn);
+
+		Debug.Log("I connected to a server!");
+	}
+
+	public override void OnServerAddPlayer(NetworkConnection conn)
+	{
+		base.OnServerAddPlayer(conn);
+
+		Debug.Log("New Player conected to a server. (" + numPlayers + " players)");
+	}
+}
